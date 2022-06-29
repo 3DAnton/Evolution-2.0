4f539cf17e15b086f68bf2718f815397c1276cf2
3DAnton <97904300+3DAnton@users.noreply.github.com>
2022-06-29 18:24:16 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -30,22 +30,98 @@ void first_message(WorldSize* w)
 
 }
 
+void reading_keyboard(sf::Event event, std::string& s,bool&vod_1,bool& vod_2,int& x)
+{
+	
+	if (event.type == sf::Event::KeyPressed)
+	{
+		switch (event.key.code)
+		{
+		case sf::Keyboard::Key::Enter:
+		{
+			vod_1 = false;
+			vod_2 = true;
+			break;
+		}
+		case sf::Keyboard::Key::Num0:
+		{
+			s.push_back('0');
+			x = x * 10;
+			break;
+		}
+		case sf::Keyboard::Key::Num1:
+		{
+			s.push_back('1');
+			x = x * 10 + 1;
+			break;
+		}
+		case sf::Keyboard::Key::Num2:
+		{
+			s.push_back('2');
+			x = x * 10 + 2;
+			break;
+		}
+		case sf::Keyboard::Key::Num3:
+		{
+			s.push_back('3');
+			x = x * 10 + 3;
+			break;
+		}
+		case sf::Keyboard::Key::Num4:
+		{
+			s.push_back('4');
+			x = x * 10 + 4;
+			break;
+		}
+		case sf::Keyboard::Key::Num5:
+		{
+			s.push_back('5');
+			x = x * 10 + 5;
+			break;
+		}
+		case sf::Keyboard::Key::Num6:
+		{
+			s.push_back('6');
+			x = x * 10 + 6;
+			break;
+		}
+		case sf::Keyboard::Key::Num7:
+		{
+			s.push_back('7');
+			x = x * 10 + 7;
+			break;
+		}
+		case sf::Keyboard::Key::Num8:
+		{
+			s.push_back('8');
+			x = x * 10 + 8;
+			break;
+		}
+		case sf::Keyboard::Key::Num9:
+		{
+			s.push_back('9');
+			x = x * 10 + 9;
+			break;
+		}
+		}
+	}
+}
 
 int first_reading(WorldSize* w)
 {
 	bool vod_x = true, vod_y = false, vod_bot = false, vod_food = false;
 	bool vod_poison=false,vod_wall=false;
-	bool vod_bot_need_evoule = false;
-	sf::RenderWindow first_mes(sf::VideoMode(1000, 800), "My window");
+	bool vod_bot_need_evoule = false,vod_cord=false;
+	sf::RenderWindow first_mes(sf::VideoMode(800, 600), "My window");
 	sf::Event event;
 
-	sf::Font font;//øðèôò 
+	sf::Font font;//шрифт 
 
 	if (!font.loadFromFile("111.ttf"))
 	{
 		std::cout << "Font load error!\n";
 	}
-	std::string s_x,s_y,s_b,s_f,s_p,s_w,s_m;
+	std::string s_x,s_y,s_b,s_f,s_p,s_w,s_m,s_cord;
 	while (first_mes.isOpen())
 	{
 		sf::Event event;
@@ -58,78 +134,8 @@ int first_reading(WorldSize* w)
 				w->need_to_error = true;
 				break;
 			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				switch (event.key.code)
-				{
-					case sf::Keyboard::Key::Enter:
-					{
-						vod_x = false;
-						vod_y = true;
-						break;
-					}
-					case sf::Keyboard::Key::Num0:
-					{
-						s_x.push_back('0');
-						(w->x) = (w->x) * 10;
-						break;
-					}
-					case sf::Keyboard::Key::Num1:
-					{
-						s_x.push_back('1');
-						(w->x) = (w->x) * 10+1;
-						break;
-					}
-					case sf::Keyboard::Key::Num2:
-					{
-						s_x.push_back('2');
-						(w->x) = (w->x) * 10 + 2;
-						break;
-					}
-					case sf::Keyboard::Key::Num3:
-					{
-						s_x.push_back('3');
-						(w->x) = (w->x) * 10 + 3;
-						break;
-					}
-					case sf::Keyboard::Key::Num4:
-					{
-						s_x.push_back('4');
-						(w->x) = (w->x) * 10 + 4;
-						break;
-					}
-					case sf::Keyboard::Key::Num5:
-					{
-						s_x.push_back('5');
-						(w->x) = (w->x) * 10 + 5;
-						break;
-					}
-					case sf::Keyboard::Key::Num6:
-					{
-						s_x.push_back('6');
-						(w->x) = (w->x) * 10 + 6;
-						break;
-					}
-					case sf::Keyboard::Key::Num7:
-					{
-						s_x.push_back('7');
-						(w->x) = (w->x) * 10 + 7;
-						break;
-					}
-					case sf::Keyboard::Key::Num8:
-					{
-						s_x.push_back('8');
-						(w->x) = (w->x) * 10 + 8;
-						break;
-					}
-					case sf::Keyboard::Key::Num9:
-					{
-						s_x.push_back('9');
-						(w->x) = (w->x) * 10 + 9;
-						break;
-					}
-				}
-			}
+			else 
+				reading_keyboard(event, s_x, vod_x, vod_y, w->x);
 			std::cout << (w->x)<<std::endl;
 			first_mes.clear();
 			sf::Text text(s_x, font, 20);
@@ -143,85 +149,14 @@ int first_reading(WorldSize* w)
 
 		while ((vod_y) && (first_mes.pollEvent(event)))
 		{
-
 			if (event.type == sf::Event::Closed)
 			{
 				first_mes.close();
 				vod_y = false;
 				w->need_to_error = true;
 			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				switch (event.key.code)
-				{
-				case sf::Keyboard::Key::Enter:
-				{
-					vod_y = false;
-					vod_bot = true;
-					break;
-				}
-				case sf::Keyboard::Key::Num0:
-				{
-					s_y.push_back('0');
-					(w->y) = (w->y) * 10;
-					break;
-				}
-				case sf::Keyboard::Key::Num1:
-				{
-					s_y.push_back('1');
-					(w->y) = (w->y) * 10 + 1;
-					break;
-				}
-				case sf::Keyboard::Key::Num2:
-				{
-					s_y.push_back('2');
-					(w->y) = (w->y) * 10 + 2;
-					break;
-				}
-				case sf::Keyboard::Key::Num3:
-				{
-					s_y.push_back('3');
-					(w->y) = (w->y) * 10 + 3;
-					break;
-				}
-				case sf::Keyboard::Key::Num4:
-				{
-					s_y.push_back('4');
-					(w->y) = (w->y) * 10 + 4;
-					break;
-				}
-				case sf::Keyboard::Key::Num5:
-				{
-					s_y.push_back('5');
-					(w->y) = (w->y) * 10 + 5;
-					break;
-				}
-				case sf::Keyboard::Key::Num6:
-				{
-					s_y.push_back('6');
-					(w->y) = (w->y) * 10 + 6;
-					break;
-				}
-				case sf::Keyboard::Key::Num7:
-				{
-					s_y.push_back('7');
-					(w->y) = (w->y) * 10 + 7;
-					break;
-				}
-				case sf::Keyboard::Key::Num8:
-				{
-					s_y.push_back('8');
-					(w->y) = (w->y) * 10 + 8;
-					break;
-				}
-				case sf::Keyboard::Key::Num9:
-				{
-					s_y.push_back('9');
-					(w->y) = (w->y) * 10 + 9;
-					break;
-				}
-				}
-			}
+			else
+				reading_keyboard(event, s_y, vod_y, vod_bot, w->y);
 			std::cout << (w->y) << std::endl;
 			first_mes.clear();
 			sf::Text text("How much world?", font, 20);
@@ -245,78 +180,8 @@ int first_reading(WorldSize* w)
 				vod_bot = false;
 				w->need_to_error = true;
 			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				switch (event.key.code)
-				{
-				case sf::Keyboard::Key::Enter:
-				{
-					vod_bot = false;
-					vod_food = true;
-					break;
-				}
-				case sf::Keyboard::Key::Num0:
-				{
-					s_b.push_back('0');
-					(w->amount_bot) = (w->amount_bot) * 10;
-					break;
-				}
-				case sf::Keyboard::Key::Num1:
-				{
-					s_b.push_back('1');
-					(w->amount_bot) = (w->amount_bot) * 10 + 1;
-					break;
-				}
-				case sf::Keyboard::Key::Num2:
-				{
-					s_b.push_back('2');
-					(w->amount_bot) = (w->amount_bot) * 10 + 2;
-					break;
-				}
-				case sf::Keyboard::Key::Num3:
-				{
-					s_b.push_back('3');
-					(w->amount_bot) = (w->amount_bot) * 10 + 3;
-					break;
-				}
-				case sf::Keyboard::Key::Num4:
-				{
-					s_b.push_back('4');
-					(w->amount_bot) = (w->amount_bot) * 10 + 4;
-					break;
-				}
-				case sf::Keyboard::Key::Num5:
-				{
-					s_b.push_back('5');
-					(w->amount_bot) = (w->amount_bot) * 10 + 5;
-					break;
-				}
-				case sf::Keyboard::Key::Num6:
-				{
-					s_b.push_back('6');
-					(w->amount_bot) = (w->amount_bot) * 10 + 6;
-					break;
-				}
-				case sf::Keyboard::Key::Num7:
-				{
-					s_b.push_back('7');
-					(w->amount_bot) = (w->amount_bot) * 10 + 7;
-					break;
-				}
-				case sf::Keyboard::Key::Num8:
-				{
-					s_b.push_back('8');
-					(w->amount_bot) = (w->amount_bot) * 10 + 8;
-					break;
-				}
-				case sf::Keyboard::Key::Num9:
-				{
-					s_b.push_back('9');
-					(w->amount_bot) = (w->amount_bot) * 10 + 9;
-					break;
-				}
-				}
-			}
+			else
+				reading_keyboard(event, s_b, vod_bot, vod_food, w->amount_bot);
 			std::cout << (w->amount_bot) << std::endl;
 			first_mes.clear();
 			sf::Text text("How much world?", font, 20);
@@ -345,78 +210,8 @@ int first_reading(WorldSize* w)
 				vod_food = false;
 				w->need_to_error = true;
 			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				switch (event.key.code)
-				{
-					case sf::Keyboard::Key::Enter:
-					{
-						vod_food = false;
-						vod_poison = true;
-						break;
-					}
-					case sf::Keyboard::Key::Num0:
-					{
-						s_f.push_back('0');
-						(w->amount_food) = (w->amount_food) * 10;
-						break;
-					}
-					case sf::Keyboard::Key::Num1:
-					{
-						s_f.push_back('1');
-						(w->amount_food) = (w->amount_food) * 10 + 1;
-						break;
-					}
-					case sf::Keyboard::Key::Num2:
-					{
-						s_f.push_back('2');
-						(w->amount_food) = (w->amount_food) * 10 + 2;
-						break;
-					}
-					case sf::Keyboard::Key::Num3:
-					{
-						s_f.push_back('3');
-						(w->amount_food) = (w->amount_food) * 10 + 3;
-						break;
-					}
-					case sf::Keyboard::Key::Num4:
-					{
-						s_f.push_back('4');
-						(w->amount_food) = (w->amount_food) * 10 + 4;
-						break;
-					}
-					case sf::Keyboard::Key::Num5:
-					{
-						s_f.push_back('5');
-						(w->amount_food) = (w->amount_food) * 10 + 5;
-						break;
-					}
-					case sf::Keyboard::Key::Num6:
-					{
-						s_f.push_back('6');
-						(w->amount_food) = (w->amount_food) * 10 + 6;
-						break;
-					}
-					case sf::Keyboard::Key::Num7:
-					{
-						s_f.push_back('7');
-						(w->amount_food) = (w->amount_food) * 10 + 7;
-						break;
-					}
-					case sf::Keyboard::Key::Num8:
-					{
-						s_f.push_back('8');
-						(w->amount_food) = (w->amount_food) * 10 + 8;
-						break;
-					}
-					case sf::Keyboard::Key::Num9:
-					{
-						s_f.push_back('9');
-						(w->amount_food) = (w->amount_food) * 10 + 9;
-						break;
-					}
-				}
-			}
+			else
+				reading_keyboard(event, s_f, vod_food, vod_poison, w->amount_food);
 			std::cout << (w->amount_food) << std::endl;
 			first_mes.clear();
 			sf::Text text("How much world?", font, 20);
@@ -451,78 +246,8 @@ int first_reading(WorldSize* w)
 				vod_poison = false;
 				w->need_to_error = true;
 			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				switch (event.key.code)
-				{
-				case sf::Keyboard::Key::Enter:
-				{
-					vod_poison = false;
-					vod_wall = true;
-					break;
-				}
-				case sf::Keyboard::Key::Num0:
-				{
-					s_p.push_back('0');
-					(w->amount_poison) = (w->amount_poison) * 10;
-					break;
-				}
-				case sf::Keyboard::Key::Num1:
-				{
-					s_p.push_back('1');
-					(w->amount_poison) = (w->amount_poison) * 10 + 1;
-					break;
-				}
-				case sf::Keyboard::Key::Num2:
-				{
-					s_p.push_back('2');
-					(w->amount_poison) = (w->amount_poison) * 10 + 2;
-					break;
-				}
-				case sf::Keyboard::Key::Num3:
-				{
-					s_p.push_back('3');
-					(w->amount_poison) = (w->amount_poison) * 10 + 3;
-					break;
-				}
-				case sf::Keyboard::Key::Num4:
-				{
-					s_p.push_back('4');
-					(w->amount_poison) = (w->amount_poison) * 10 + 4;
-					break;
-				}
-				case sf::Keyboard::Key::Num5:
-				{
-					s_p.push_back('5');
-					(w->amount_poison) = (w->amount_poison) * 10 + 5;
-					break;
-				}
-				case sf::Keyboard::Key::Num6:
-				{
-					s_p.push_back('6');
-					(w->amount_poison) = (w->amount_poison) * 10 + 6;
-					break;
-				}
-				case sf::Keyboard::Key::Num7:
-				{
-					s_p.push_back('7');
-					(w->amount_poison) = (w->amount_poison) * 10 + 7;
-					break;
-				}
-				case sf::Keyboard::Key::Num8:
-				{
-					s_p.push_back('8');
-					(w->amount_poison) = (w->amount_poison) * 10 + 8;
-					break;
-				}
-				case sf::Keyboard::Key::Num9:
-				{
-					s_p.push_back('9');
-					(w->amount_poison) = (w->amount_poison) * 10 + 9;
-					break;
-				}
-				}
-			}
+			else
+				reading_keyboard(event, s_p, vod_poison, vod_wall, w->amount_poison);
 			std::cout << (w->amount_poison) << std::endl;
 			first_mes.clear();
 			sf::Text text("How much world?", font, 20);
@@ -563,78 +288,8 @@ int first_reading(WorldSize* w)
 				vod_wall = false;
 				w->need_to_error = true;
 			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				switch (event.key.code)
-				{
-				case sf::Keyboard::Key::Enter:
-				{
-					vod_wall = false;
-					vod_bot_need_evoule = true;
-					break;
-				}
-				case sf::Keyboard::Key::Num0:
-				{
-					s_w.push_back('0');
-					(w->amount_wall) = (w->amount_wall) * 10;
-					break;
-				}
-				case sf::Keyboard::Key::Num1:
-				{
-					s_w.push_back('1');
-					(w->amount_wall) = (w->amount_wall) * 10 + 1;
-					break;
-				}
-				case sf::Keyboard::Key::Num2:
-				{
-					s_w.push_back('2');
-					(w->amount_wall) = (w->amount_wall) * 10 + 2;
-					break;
-				}
-				case sf::Keyboard::Key::Num3:
-				{
-					s_w.push_back('3');
-					(w->amount_food) = (w->amount_food) * 10 + 3;
-					break;
-				}
-				case sf::Keyboard::Key::Num4:
-				{
-					s_w.push_back('4');
-					(w->amount_wall) = (w->amount_wall) * 10 + 4;
-					break;
-				}
-				case sf::Keyboard::Key::Num5:
-				{
-					s_w.push_back('5');
-					(w->amount_wall) = (w->amount_wall) * 10 + 5;
-					break;
-				}
-				case sf::Keyboard::Key::Num6:
-				{
-					s_w.push_back('6');
-					(w->amount_wall) = (w->amount_wall) * 10 + 6;
-					break;
-				}
-				case sf::Keyboard::Key::Num7:
-				{
-					s_w.push_back('7');
-					(w->amount_wall) = (w->amount_wall) * 10 + 7;
-					break;
-				}
-				case sf::Keyboard::Key::Num8:
-				{
-					s_w.push_back('8');
-					(w->amount_wall) = (w->amount_wall) * 10 + 8;
-					break;
-				}
-				case sf::Keyboard::Key::Num9:
-				{
-					s_w.push_back('9');
-					(w->amount_wall) = (w->amount_wall) * 10 + 9;
-					break;
-				}
-				}
-			}
+			else
+				reading_keyboard(event, s_w, vod_wall, vod_cord, w->amount_wall);
 			std::cout << (w->amount_wall) << std::endl;
 			first_mes.clear();
 			sf::Text text("How much world?", font, 20);
@@ -672,6 +327,76 @@ int first_reading(WorldSize* w)
 			first_mes.draw(text_w);
 			first_mes.display();
 		}
+		while ((vod_cord) && (first_mes.pollEvent(event)))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				first_mes.close();
+				vod_cord = false;
+				w->need_to_error = true;
+			}
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Key::Enter)
+				{
+					vod_cord = false;
+					vod_bot_need_evoule = true;
+				}
+				if (event.key.code == sf::Keyboard::Key::Num0)
+				{
+					w->need_to_cordinat = false;
+					s_cord.push_back('0');
+				}
+				if (event.key.code == sf::Keyboard::Key::Num1)
+				{
+					w->need_to_cordinat = true;
+					s_cord.push_back('1');
+				}
+			}
+				std::cout << (w->need_to_cordinat) << std::endl;
+				first_mes.clear();
+				sf::Text text("How much world?", font, 20);
+				sf::Text text_x(s_x, font, 20);
+				sf::Text text_y(s_y, font, 20);
+				sf::Text text_2("How much bot?", font, 20);
+				sf::Text text_b(s_b, font, 20);
+				sf::Text text_3("How much food?", font, 20);
+				sf::Text text_f(s_f, font, 20);
+				sf::Text text_4("How much poison?", font, 20);
+				sf::Text text_p(s_p, font, 20);
+				sf::Text text_5("How much wall?", font, 20);
+				sf::Text text_w(s_w, font, 20);
+				sf::Text text_6("Need to cordinates(1 - Yes, 0 - No)", font, 20);
+				sf::Text text_cord(s_cord, font, 20);
+				text.setPosition(0, 0);
+				text_x.setPosition(0, 20);
+				text_y.setPosition(0, 40);
+				text_2.setPosition(0, 60);
+				text_b.setPosition(0, 80);
+				text_3.setPosition(0, 100);;
+				text_f.setPosition(0, 120);
+				text_4.setPosition(0, 140);
+				text_p.setPosition(0, 160);
+				text_5.setPosition(0, 180);
+				text_w.setPosition(0, 200);
+				text_6.setPosition(0, 220);
+				text_cord.setPosition(0, 240);
+				first_mes.draw(text);
+				first_mes.draw(text_x);
+				first_mes.draw(text_y);
+				first_mes.draw(text_2);
+				first_mes.draw(text_b);
+				first_mes.draw(text_3);
+				first_mes.draw(text_f);
+				first_mes.draw(text_4);
+				first_mes.draw(text_p);
+				first_mes.draw(text_5);
+				first_mes.draw(text_w);
+				first_mes.draw(text_6);
+				first_mes.draw(text_cord);
+
+				first_mes.display();
+		}
 		while ((vod_bot_need_evoule) && (first_mes.pollEvent(event)))
 		{
 
@@ -683,74 +408,18 @@ int first_reading(WorldSize* w)
 			}
 			if (event.type == sf::Event::KeyPressed)
 			{
-				switch (event.key.code)
-				{
-				case sf::Keyboard::Key::Enter:
+				if (event.key.code == sf::Keyboard::Key::Enter)
 				{
 					vod_bot_need_evoule = false;
+					first_mes.close();
+					w->need_to_error = false;
 					return 1;
-					break;
 				}
-				case sf::Keyboard::Key::Num0:
+				else
 				{
-					s_m.push_back('0');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10;
-					break;
-				}
-				case sf::Keyboard::Key::Num1:
-				{
-					s_m.push_back('1');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 1;
-					break;
-				}
-				case sf::Keyboard::Key::Num2:
-				{
-					s_w.push_back('2');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 2;
-					break;
-				}
-				case sf::Keyboard::Key::Num3:
-				{
-					s_m.push_back('3');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 3;
-					break;
-				}
-				case sf::Keyboard::Key::Num4:
-				{
-					s_m.push_back('4');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 4;
-					break;
-				}
-				case sf::Keyboard::Key::Num5:
-				{
-					s_m.push_back('5');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 5;
-					break;
-				}
-				case sf::Keyboard::Key::Num6:
-				{
-					s_m.push_back('6');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 6;
-					break;
-				}
-				case sf::Keyboard::Key::Num7:
-				{
-					s_m.push_back('7');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 7;
-					break;
-				}
-				case sf::Keyboard::Key::Num8:
-				{
-					s_m.push_back('8');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 8;
-					break;
-				}
-				case sf::Keyboard::Key::Num9:
-				{
-					s_m.push_back('9');
-					(w->amount_boot_evolue) = (w->amount_boot_evolue) * 10 + 9;
-					break;
-				}
+					bool q1 = true, q2 = true;
+					reading_keyboard(event, s_m, q1, q2, w->amount_boot_evolue);
+					
 				}
 			}
 			std::cout << (w->amount_wall) << std::endl;
@@ -766,7 +435,9 @@ int first_reading(WorldSize* w)
 			sf::Text text_p(s_p, font, 20);
 			sf::Text text_5("How much wall?", font, 20);
 			sf::Text text_w(s_w, font, 20);
-			sf::Text text_6("How much bot need to evolue?",font,20);
+			sf::Text text_6("Need to cordinates(1 - Yes, 0 - No)", font, 20);
+			sf::Text text_cord(s_cord, font, 20);
+			sf::Text text_7("How much boot need to evolue?", font, 20);
 			sf::Text text_m(s_m, font, 20);
 			text.setPosition(0, 0);
 			text_x.setPosition(0, 20);
@@ -780,7 +451,9 @@ int first_reading(WorldSize* w)
 			text_5.setPosition(0, 180);
 			text_w.setPosition(0, 200);
 			text_6.setPosition(0, 220);
-			text_m.setPosition(0, 240);
+			text_cord.setPosition(0, 240);
+			text_7.setPosition(0, 260);
+			text_m.setPosition(0, 280);
 			first_mes.draw(text);
 			first_mes.draw(text_x);
 			first_mes.draw(text_y);
@@ -793,10 +466,15 @@ int first_reading(WorldSize* w)
 			first_mes.draw(text_5);
 			first_mes.draw(text_w);
 			first_mes.draw(text_6);
+			first_mes.draw(text_cord);
+			first_mes.draw(text_7);
 			first_mes.draw(text_m);
 			first_mes.display();
 		}
+		
+		
 	}
+	return 1;
 }
 
 
@@ -806,18 +484,14 @@ int main()
 	WorldSize w;
 	first_reading(&w);
 	//if(!(w.otkladka)) first_message(&w);
+	int s1 = ((w.x) - 1) * ((w.y) - 1), s2 = (w.amount_bot) + (w.amount_food) + (w.amount_poison) + (w.amount_wall);
+	if (s1 < s2) w.need_to_error = 1;
 	if (w.need_to_error)
 	{
 		std::cout << "ERROR";
 		return 0;
 	}
 
-        
     God g(&w);
 	g.run_2(&w);
-
-
-
-
-
 }
