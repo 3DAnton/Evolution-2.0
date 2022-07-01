96d62c34015a8477cf1b9f7739c589a03ba14c2f
3DAnton <97904300+3DAnton@users.noreply.github.com>
2022-07-01 16:43:42 +0300

    Update sas.hpp

--- sas.hpp
+++ sas.hpp
@@ -1,19 +1,19 @@
-#ifndef SAS_HPP
+ #ifndef SAS_HPP
 #define SAS_HPP
 
 
 struct WorldSize
 {
-	bool otkladka = 0;
-	int x = 0, y = 0;
-	int amount_bot = 0, amount_food = 0, amount_poison = 0, amount_wall = 0, amount_boot_evolue = 0;
+	bool otkladka = 1;
+	int x = 10, y = 10;
+	int amount_bot = 10, amount_food = 0, amount_poison = 0, amount_wall = 0, amount_boot_evolue = 1;
 	bool make_a_pause = true, need_to_draw = true, need_to_pause = false;
 	bool q, mIsTurnedOff = false, need_to_draw_graph = false;
 	int pause_delta = 100;
 	int pause_time = 10e3;
 	int x_draw = 0, y_draw = 0;
 	bool need_to_graph = false, need_to_move = true, need_to_cordinat = false;
-	bool mas_1 = false, mas_2 = true, mas_3 = false;
+	bool mas_1 = false, mas_2 = false, mas_3 = false,mas_4=true;
 	bool move_up = false, move_down = false, move_right = false, move_left = false;
 	bool need_to_error = false;
 	char pause = 'X';
